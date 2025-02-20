11445b6d91937e425ae41149fae82e78b78cb4b3
iadev2005 <idaliendres.23@est.ucab.edu.ve>
2025-02-20 08:12:43 -0400

    1era parte del curso, datos simples

---
+++ 1-Tipos-de-Datos/tipos-de-datos.py
@@ -0,0 +1,21 @@
+# Cadenas
+"String"
+'string'
+
+"""Tus datos son:
+    Nombre: Ignacio
+    Apellido: Aliendres
+"""
+    
+'''Tus datos son:
+    Nombre: Ignacio
+    Apellido: Aliendres
+'''
+
+# Datos numéricos
+40
+40.2
+
+# Booleanos
+False
+True
