7b4f264f46f3208f03c7d2485b2d0cb4c8f24da0
iadev2005 <idaliendres.23@est.ucab.edu.ve>
2025-02-20 15:39:16 -0400

    Trabajando con inputs

---
+++ 6-Inputs/input-nombres.py
@@ -0,0 +1,7 @@
+# Pedirle un datos al usuario
+nombre = input("Cual es tu nombre?: ")
+
+# El dato que nos devuelve input siempre es un texto
+
+# Mostrando el nombre
+print(f"Tu nombre es: {nombre}")
---
+++ 6-Inputs/input-numeros.py
@@ -0,0 +1,12 @@
+# Pedirle un número al usuario
+numero = input("dame un numero, capitan: ")
+
+# Multiplicacndo el número por 2 y lo volvemos un entero
+# resultado = int(numero)*2
+
+# Multiplicacndo el número por 2 y lo volvemos un entero
+resultado = float(numero)*2
+
+# Mostrando el resultado
+print(f"El resultado es: {resultado}")
+
