efc50550ff59ebcea33621638533068942e2a858
iadev2005 <idaliendres.23@est.ucab.edu.ve>
2025-02-20 09:31:27 -0400

    Aprendiendo sobre los operadores comparacionales en Python

---
+++ 3-Operadores/op-comparacion.py
@@ -0,0 +1,24 @@
+es_igual_que = 5 == 6 # Falso 5 no es igual a 6
+es_distinto_que = 5 != 6 # Verdadero 5 no es igual a 6
+mayor_que = 5 > 6 # Falso
+menor_que = 5 < 6 # Verdadero
+mayor_o_igual = 5 >= 6 # Falso
+menor_o_igual = 5 <= 6 # Verdadero
+
+print(es_igual_que)
+print(es_distinto_que)
+print(mayor_que)
+# ...
+
+# Calculos Combinados
+a = 5
+b = 10
+c = 20
+comparacion= a + b < c
+ 
+print(comparacion) 
+
+# Comparar Usuariops
+contraseña_almacenada = "IgnacioMaestro"
+contraseña_escrita = "DaltoMaestro" 
+print(contraseña_almacenada == contraseña_escrita) # Esto es falso
