dc69a201358b09820c40aa90cc3e89e7ff6f7dd9
iadev2005 <idaliendres.23@est.ucab.edu.ve>
2025-02-20 10:10:25 -0400

    Operadores Logicos en Python

---
+++ 3-Operadores/op-logicos.py
@@ -0,0 +1,17 @@
+# AND
+r1= True & True # Devuelve True
+r2= False & True # Devuelve False
+r3= True & False # Devuelve False
+r4=False & False # Devuelve False
+
+# OR
+r5= True | True # Devuelve True
+r6= False | True # Devuelve True
+r7= True | False # Devuelve True
+r8= False | False # Devuelve False
+
+# NOT
+r9= not True # Devuelve False
+r10= not False # Devuelve True
+
+print(r1)
